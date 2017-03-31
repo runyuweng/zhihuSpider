ff721410ac5b58d1459f40a7ef58ed25edb0afb0
wengwengweng <593110501@qq.com>
2017-03-31 19:49:36 +0800

    update

--- spider/db/db.js
+++ spider/db/db.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : '115.159.85.254',
-  user     : 'root',
-  password : 'WW593110501',
-  database : 'zhihu'
+  host     : '',
+  user     : '',
+  password : '',
+  database : ''
 });
 
 module.exports = connection;
