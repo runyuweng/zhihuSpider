6d82ce4bb918402d5f59eed8b3ff84c82cc5ca3e
wengwengweng <593110501@qq.com>
2017-04-01 13:12:28 +0800

    update

--- spider/master.js
+++ spider/master.js
@@ -1,5 +1,5 @@
 var fork = require('child_process').fork;
 var cpus = require('os').cpus();
 for(var i = 0; i < 2; i++){
-    fork('./index.js').send({'num':i});
+    fork('./spider/index.js').send({'num':i});
 }
